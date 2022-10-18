964a779058ceee14aebc28da3868bf66b3c3f1fb
Luigi Paschoalino <64054620+luigi-paschoalino@users.noreply.github.com>
2022-10-18 15:29:13 -0300

    pacotes para conexao bd

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const port = 3000;
 
 app.use(express.static(__dirname + '/styles'));
 app.use(express.static(__dirname + '/scripts'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/login.html'));
 });
 
 app.post('/home', function (req, res) {
+    console.log(req.body.matricula);
+    console.log(req.body.password);
     res.sendFile(path.join(__dirname + '/views/tela-inicial.html'));
 });
 
@@ -44,6 +52,10 @@ app.get('/createCRM', function (req, res) {
 
 app.get('/addUser', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/cadastrar-usuario.html'));
+});
+
+app.get('/teste', function (req, res) {
+    res.send('<h1>Funfou</h1>');
 });
 
 // Ligando o servidor
